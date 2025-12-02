ca13983405a63467a9c3e3eb2ad7fbeb7bad718b
Mona Lisa <fpiller@gmail.com>
2025-12-02 11:36:12 +0100

    3

--- szovegeles_02.py
+++ szovegeles_02.py
@@ -26,9 +26,22 @@ for i in  betu:
 print (db_szam)
 
 print (db_betu)
+#3. feladat
+speclista=[]
 
+for i in betu:
+    if i in string.punctuation and i not in speclista:
+        speclista.append(i)
 
-
+try:
+    with open("string_output.txt","a",encoding = 'utf-8')as fajl:
+        for i in len(speclista):
+            if i<len(speclista)-1 :
+                fajl.write(speclista[i] + ", ")
+            else:
+                fajl.write(speclista[i])
+except IOError as ex:
+    print(ex)
 
 
 
