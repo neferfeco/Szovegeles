4f6a16990cdaa8094598f3e86a1378a0de6a5636
Mona Lisa <fpiller@gmail.com>
2025-12-11 10:35:03 +0100

    szavak_karakterek

---
+++ szovegeles_03.py
@@ -0,0 +1,68 @@
+#1. feladat
+szoveg = ''
+try:
+    with open ("input.txt", "r", encoding="utf-8") as file:
+        szoveg = file.read()
+except IOError as hiba:
+    print(hiba)
+
+def feladat1():
+    szoveg_szelet = szoveg.strip().split('\n')
+    karakterek = 0
+        
+    for szoveg1 in szoveg_szelet:
+        karakterek += len(szoveg1)
+    
+    return karakterek
+
+def feladat1_2():
+    szavak = szoveg.split(' ')
+    
+    return len(szavak)
+
+try:
+    with open ("output.txt", "w", encoding="utf-8") as file:
+        file.write(f"1.feladat: {feladat1()}    \n")
+        file.write(f"1_2. feladat: {feladat1_2()}    \n")
+except IOError as hiba:
+    print(hiba)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
