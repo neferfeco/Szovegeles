a9edbce3b045e87d4fcfcee2e69116847280906a
Mona Lisa <fpiller@gmail.com>
2026-01-15 11:38:56 +0100

    2026-01-15

--- F3/szovegeles_03.py
+++ F3/szovegeles_03.py
@@ -48,10 +48,43 @@ def feladat4():
 
     return kulcsszo
 
-            
-            
-        
-       
+def feladat5():
+    global szoveg
+    
+    db = szoveg.count("MI")
+    
+    szoveg = szoveg.replace("MI", "Mesterséges Intelligencia")
+    
+    return db    
+
+
+def leghosszabb_mondat():
+    mondatok = []
+    
+    # Üres sorok eltávolítása
+    szoveg2 = szoveg.strip().replace("\n", " ")
+    
+    
+    j = 0    
+    for i in range(0, len(szoveg2)):
+        if szoveg2[i] == "." or szoveg2[i] == "?" or szoveg2[i] == "!":            
+            mondatok.append(szoveg2[j:i+1])
+            j = i + 2
+    
+    # MAXIMUM KIVÁLASZTÁS
+    max_i = 0
+    
+    for i in range(1, len(mondatok)):
+        if len(mondatok[i]) > len(mondatok[max_i]):
+            max_i = i
+    
+    return mondatok[max_i]
+
+
+
+
+
+
 
 # ****************************
 #  A PROGRAM
@@ -59,15 +92,18 @@ def feladat4():
 
 try:
     with open ("output.txt", "w", encoding="utf-8") as file:
-        file.write(f"1.feladat: {feladat1()}    \n")
-        file.write(f"1_2. feladat: {feladat1_2()}    \n")
-        file.write(f"4 feladat: {sorted(feladat4())}   \n")
+        file.write(f"1.feladat: {feladat1()}\n")
+        file.write(f"1_2. feladat: {feladat1_2()}\n")
+        file.write(f"4 feladat: {sorted(feladat4())}\n")
+        file.write(f"{feladat5()}* fordul elő a szövegben az MI szó.\n")
+        file.write(f"A leghosszabb mondat:\n\t{leghosszabb_mondat()}")
 except IOError as hiba:
     print(hiba)
     
-print(feladat2())
+#print(feladat2())
 
 
+    
 
 
 
